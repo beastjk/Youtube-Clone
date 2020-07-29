c38abf9c311f56f3902b6570fcd2adb21cc0adfd
Jay Patel <jay.p1@ahduni.edu.in>
2020-07-30 01:15:19 +0530

    2nd commit

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './SidebarRow.css'
 import HomeIcon from '@material-ui/icons/Home';
 
-function SidebarRow({title, Icon}) {
+function SidebarRow({selected, title, Icon}) {
     return (
-        <div className = 'sidebarRow'>
-            <Icon className = "sidebarRow__icon"/>
+        <div className={`sidebarRow ${selected && ' selected'}`}>
+            <Icon className = {'sidebarRow__icon'}/>
             <h2 className="sidebarRow__title">{title}</h2>
         </div>
     )
