c091b3b8e86b55d5ac3ee0407c5221c35e58f8d9
Jay Patel <jay.p1@ahduni.edu.in>
2020-07-30 10:10:35 +0530

    Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,11 @@ import SidebarRow from './SidebarRow'
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import HomeIcon from '@material-ui/icons/Home';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
 
 
 function Sidebar() {
@@ -13,6 +18,12 @@ function Sidebar() {
             <SidebarRow Icon={WhatshotIcon} title='Trending' />
             <SidebarRow Icon={SubscriptionsIcon} title='Subscription' />
             <hr/>
+            <SidebarRow Icon={VideoLibraryIcon} title='Library' />
+            <SidebarRow Icon={HistoryIcon} title='Trending' />
+            <SidebarRow Icon={SubscriptionsIcon} title='History' />
+            <SidebarRow Icon={OndemandVideoIcon} title='Watch Later' />
+            <SidebarRow Icon={ThumbUpAltOutlinedIcon} title='Liked Videos' />
+            <SidebarRow Icon={ExpandMoreOutlinedIcon} title='Show More' />
         </div>
     )
 }
